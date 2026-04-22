cf5ef823ddd03a3ff6c21b8c3563c9f3d59bc034
Diwash Bhandari <diwashbhandari88@gmail.com>
2026-04-22 17:08:03 +0300

    Restructure project into professor template layout

--- frontend/src/components/StatusMessage.jsx
+++
@@ -1,9 +0,0 @@
-function StatusMessage({ type = "info", children }) {
-  if (!children) {
-    return null;
-  }
-
-  return <p className={`status-message status-message--${type}`}>{children}</p>;
-}
-
-export default StatusMessage;
